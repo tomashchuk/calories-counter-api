dcbf7d80c3f19747e5f55c8f73f90fae8677577a
tomashchuk <tomashchukdmytro@gmail.com>
2021-11-27 22:15:18 +0200

    add access_token to create user response

--- calculator/serializers.py
+++ calculator/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model # If used custom user model
+from django.contrib.auth import get_user_model  # If used custom user model
+from rest_framework_simplejwt.tokens import RefreshToken
 
 UserModel = get_user_model()
 
@@ -7,6 +8,7 @@ UserModel = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True)
+    access_token = serializers.SerializerMethodField()
 
     def create(self, validated_data):
 
@@ -14,10 +16,11 @@ class UserSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             password=validated_data['password'],
         )
-
         return user
 
     class Meta:
         model = UserModel
-        # Tuple of serialized model fields (see link [2])
-        fields = ("id", "username", "password", )
+        fields = ("id", "username", "password", "access_token", )
+
+    def get_access_token(self, obj):
+        return str(RefreshToken.for_user(obj).access_token)
